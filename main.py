716434f3730ca2a09a39d739d333dc4b5c99e599
fiafob <fiafob@ya.ru>
2023-05-20 23:33:14 +0300

    it works 2

--- main.py
+++ main.py
@@ -19,10 +19,6 @@ db_session.global_init(user_name, user_password, host_name, port, database_name)
 db_sess = db_session.create_session()
 
 
-def collect_data(source: str, data: str) -> list:
-    raw_data = json.loads(data)
-
-
 # Returns list of dictionaries with key, value: table_column, table_value
 def get_data_from_db(table_type: table_types) -> list:
     result = db_sess.query(table_type).all()
@@ -38,9 +34,46 @@ def set_data_to_db(*args: table_types):
         db_sess.add(data)
     db_sess.commit()
 
-def main():
-    ...
 
+import sys
+
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QMessageBox,
+    QTableWidget,
+    QTableWidgetItem,
+)
+
+class PassengersView(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("QTableView Example")
+        self.resize(750, 650)
+        headings = ["id", "first_name", "last_name", "patronymic", "passport_number",
+             "flight_id", "seat_id"]
+        # Set up the view and load the data
+        self.view = QTableWidget()
+        self.view.setColumnCount( len(headings) )
+        self.view.setHorizontalHeaderLabels(headings)
+
+        self.view.setRowCount(db_sess.query(Passenger).count())
+        columns = self.view.columnCount()
+        data = get_data_from_db(Passenger)
+
+        for j in range(len(data)):
+            for i in range(columns):
+                print([headings[i]])
+                self.view.setItem(j, i, QTableWidgetItem(str(data[j].get(headings[i], None))))
+        self.view.resizeColumnsToContents()
+        self.setCentralWidget(self.view)
+
+def main():
+    app = QApplication(sys.argv)
+    win = PassengersView()
+    win.show()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
